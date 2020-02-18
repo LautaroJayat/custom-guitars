688dbddef15200bbc0cd4c8daeab6ad736117b21
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-17 23:46:59 -0300

    Updates

---
+++ precache-manifest.09b2f2f8ffb772f1befbde6e3c0ea69a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dd76318b9f702e3640e70cec444915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e75d9635837b64a9de",
+    "url": "/static/css/main.bc66b29f.chunk.css"
+  },
+  {
+    "revision": "01113c7bfc97f77a6024",
+    "url": "/static/js/2.4e35303c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4e35303c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e75d9635837b64a9de",
+    "url": "/static/js/main.61ae8af9.chunk.js"
+  },
+  {
+    "revision": "21d4ba398de1f61c6608",
+    "url": "/static/js/runtime-main.e610a5db.js"
+  },
+  {
+    "revision": "6fa5a9d5466a928872b17b5207f8c4fd",
+    "url": "/static/media/logoSVG.6fa5a9d5.svg"
+  }
+]);
