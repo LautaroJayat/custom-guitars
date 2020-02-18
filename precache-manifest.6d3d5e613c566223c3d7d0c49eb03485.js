081b086794c0838ecb3caf8ab025a8f1a1ee9598
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-17 23:50:36 -0300

    Updates

---
+++ precache-manifest.6d3d5e613c566223c3d7d0c49eb03485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cf07a24f314bce557d8187e104b03d",
+    "url": "/custom-guitars/index.html"
+  },
+  {
+    "revision": "04326c9cf29eb8205a23",
+    "url": "/custom-guitars/static/css/main.bc66b29f.chunk.css"
+  },
+  {
+    "revision": "01113c7bfc97f77a6024",
+    "url": "/custom-guitars/static/js/2.4e35303c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/custom-guitars/static/js/2.4e35303c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04326c9cf29eb8205a23",
+    "url": "/custom-guitars/static/js/main.4be4d75e.chunk.js"
+  },
+  {
+    "revision": "439a82166dbbf1d5f77e",
+    "url": "/custom-guitars/static/js/runtime-main.11fe91fc.js"
+  },
+  {
+    "revision": "6fa5a9d5466a928872b17b5207f8c4fd",
+    "url": "/custom-guitars/static/media/logoSVG.6fa5a9d5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09b2f2f8ffb772f1befbde6e3c0ea69a.js"
+  "/custom-guitars/precache-manifest.6d3d5e613c566223c3d7d0c49eb03485.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/custom-guitars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
