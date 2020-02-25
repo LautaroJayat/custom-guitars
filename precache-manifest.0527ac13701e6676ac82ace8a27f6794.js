c3276b5651e5ed93ab4bddb7ecfeb3f1a499d3cb
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-02-25 20:26:19 -0300

    Updates

---
+++ precache-manifest.0527ac13701e6676ac82ace8a27f6794.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5963ae70a2323783b12bfeb9d07d75d",
+    "url": "/custom-guitars/index.html"
+  },
+  {
+    "revision": "befb29c8fd4eb860841d",
+    "url": "/custom-guitars/static/css/main.67f2bdbf.chunk.css"
+  },
+  {
+    "revision": "5dc8e3c85a95d061bf30",
+    "url": "/custom-guitars/static/js/2.73a5941b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/custom-guitars/static/js/2.73a5941b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befb29c8fd4eb860841d",
+    "url": "/custom-guitars/static/js/main.71a94ca2.chunk.js"
+  },
+  {
+    "revision": "439a82166dbbf1d5f77e",
+    "url": "/custom-guitars/static/js/runtime-main.11fe91fc.js"
+  },
+  {
+    "revision": "8adf86fcbce395ed6fa0bfb0ced28a4c",
+    "url": "/custom-guitars/static/media/bass.8adf86fc.png"
+  },
+  {
+    "revision": "fe9a637635089e43134bf1fd0db846fb",
+    "url": "/custom-guitars/static/media/case.fe9a6376.png"
+  },
+  {
+    "revision": "5c00d6104b2115b13232e8aff1e825b4",
+    "url": "/custom-guitars/static/media/classical.5c00d610.png"
+  },
+  {
+    "revision": "dadff2b63534bb5dbefdeda06cccdc18",
+    "url": "/custom-guitars/static/media/hat.dadff2b6.png"
+  },
+  {
+    "revision": "6fa5a9d5466a928872b17b5207f8c4fd",
+    "url": "/custom-guitars/static/media/logoSVG.6fa5a9d5.svg"
+  },
+  {
+    "revision": "dd0c5623c2f43be893fb0ad954807657",
+    "url": "/custom-guitars/static/media/metal.dd0c5623.png"
+  },
+  {
+    "revision": "752af59f868dec48102eb2770648bb63",
+    "url": "/custom-guitars/static/media/paypal.752af59f.png"
+  },
+  {
+    "revision": "03e6f9c3c27a30628c325b7704159ef7",
+    "url": "/custom-guitars/static/media/shirt.03e6f9c3.png"
+  },
+  {
+    "revision": "0e9a057f2ff3792936f78e60b39e901f",
+    "url": "/custom-guitars/static/media/strato.0e9a057f.png"
+  }
+]);
